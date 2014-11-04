76000042a57b49394a32c5d939b779df127c1e28
chen.goldberg@gmail.com <chen.goldberg@gmail.com@f3a8aef2-8191-6c73-71bc-b8b8444fef4c>
2014-11-04 23:02:08 +0000

    Added ex27: Bezier curves Tessellation

---
+++ trunk/OpenGL/res/shader/ex27.TCS.glsl
@@ -0,0 +1,14 @@
+﻿#version 400 core
+layout(vertices = 4) out;
+uniform mat4 uModelViewMatrix;
+uniform mat4 uProjectionMatrix;
+
+void main()
+{
+	vec4 vec = uProjectionMatrix*uModelViewMatrix*(gl_in[0].gl_Position - gl_in[3].gl_Position);
+	float N = length(vec.xyz);
+
+	gl_TessLevelOuter[0] = 1;
+	gl_TessLevelOuter[1] = 40*N;
+	gl_out[gl_InvocationID].gl_Position = gl_in[gl_InvocationID].gl_Position;
+}
---
+++ trunk/OpenGL/res/shader/ex27.TES.glsl
@@ -0,0 +1,21 @@
+﻿#version 400 core
+layout(isolines, equal_spacing, ccw/*, point_mode*/ ) in;
+uniform mat4 uModelViewMatrix;
+uniform mat4 uProjectionMatrix;
+
+vec4 sampleCurve(float u)
+{
+	return 
+		(1-u)*(1-u)*(1-u) * gl_in[0].gl_Position + 
+		3*(1-u)*(1-u)*u * gl_in[1].gl_Position + 
+		3*(1-u)*u*u * gl_in[2].gl_Position + 
+		u*u*u * gl_in[3].gl_Position;
+}
+
+void main()
+{
+    //vec4 position = mix(gl_in[0].gl_Position, gl_in[3].gl_Position, gl_TessCoord.x);
+	vec4 position = sampleCurve(gl_TessCoord.x);
+	gl_Position = uProjectionMatrix * uModelViewMatrix * position;
+	gl_PointSize = 5;
+}
